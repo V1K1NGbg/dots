2cb72433dd1838f1b38bd60d655c2eda7c9b0b39
V1K1NGbg <victor@ilchev.com>
2024-10-21 00:48:00 +0200

    changes to install

--- install.sh
+++ install.sh
@@ -2,6 +2,12 @@
 
 # sudo usbimager
 
+# extra packages: git vim firefox
+
+git clone https://github.com/V1K1NGbg/dots.git
+cd dots
+# vim install.sh
+
 # update
 sudo pacman -Syu
 
@@ -20,8 +26,21 @@ sudo pacman -Rs awesome
 # install the latest
 paru -S awesome-git
 
-# install packages
-paru -S acpi alacritty alsa-utils arandr autorandr bash-completi1on blueman bluez bluez-utils capitaine-cursors cowsay cpupower-gui-git curl discord docker dracut fd firefox flameshot fzf gimp git github-cli glava gnome-boxes gnome-disk-utility htop i3lock-color jdk21-openjdk jdk8-openjdk keepassxc less lolcat man-db man-pages nano nemo neofetch network-manager-applet obs-studio parsec-bin pasystray pavucontrol pcloud-drive plymouth plymouth-theme-hexagon-hud-git prismlauncher ranger remmina rofi rofi-calc spotify-launcher tmux tree qt6-svg unzip usbimager-x11 vim visual-studio-code-bin wget xdotool xss-lock zip
+# install packages (if using vim with no copy you can use cat)
+paru -S acpi alacritty alsa-utils arandr autorandr bash-completion blueman bluez bluez-utils capitaine-cursors cowsay cpupower-gui-git curl discord docker dracut fd firefox flameshot fzf gimp git github-cli glava gnome-boxes gnome-disk-utility htop i3lock-color jdk21-openjdk jdk8-openjdk keepassxc less lolcat man-db man-pages nano nemo neofetch network-manager-applet obs-studio parsec-bin pasystray pavucontrol pcloud-drive plymouth plymouth-theme-hexagon-hud-git prismlauncher ranger remmina rofi rofi-calc ruby-fusuma spotify-launcher tmux tree qt6-svg unzip usbimager-x11 vim visual-studio-code-bin wget xdotool xss-lock zip
+
+# auto login
+sudo vim /etc/systemd/system/getty.target.wants/getty\@tty1.service
+
+#ExecStart=-/sbin/agetty -o '-p -- \\u' --noclear - $TERM
+ExecStart=-/sbin/agetty -a victor - $TERM
+
+# add to ~/.bash_profile
+vim ~/.bash_profile
+
+if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
+startx
+fi
 
 # pcom
 git clone https://github.com/pijulius/picom.git
@@ -29,6 +48,8 @@ cd picom
 meson setup --buildtype=release build
 ninja -C build
 ninja -C build install
+cd ..
+sudo rm -r picom/
 
 # plymouth
 sudo vim /boot/loader/entries/{TAB}.conf
@@ -47,6 +68,8 @@ gh auth login
 
 # pcloud
 # sign in (Google + Auth)
+mkdir -p ~/Documents/BackUp
+mkdir -p ~/Documents/PC
 # Backup ~/Documents/BackUp
 # Sync ~/Documents/PC <-> pCloudDrive/PC
 
@@ -64,6 +87,8 @@ nvm install node
 # install oh-my-bash
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 
+# !!! ONLY FON NVIDIA GPU'S !!!
+
 # install nvidia drivers (https://wiki.archlinux.org/title/NVIDIA)
 paru -S nvidia-open nvidia-utils nvidia-settings
 # install requirements for gpu enabling/disabling gpu
@@ -74,6 +99,9 @@ sudo envycontrol -s integrated
 
 # enable tap clicking, natural scrolling and double tap
 # in (/usr/share/X11/xorg.conf.d/40-libinput.conf)
+
+sudo vim /usr/share/X11/xorg.conf.d/40-libinput.conf
+
 # Section "InputClass"
 #     Identifier "... touchpad ..."
 #     ...
@@ -85,6 +113,9 @@ sudo envycontrol -s integrated
 
 # enable bg layout
 # in (/etc/X11/xorg.conf.d/00-keyboard.conf)
+
+sudo vim /etc/X11/xorg.conf.d/00-keyboard.conf
+
 # Section "InputClass"
 #         Identifier "system-keyboard"
 #         MatchIsKeyboard "on"
@@ -102,7 +133,7 @@ mv ~/Downloads/Monocraft-nerd-fonts-patched.ttc ~/.local/share/fonts/
 # reload the fonts
 fc-cache
 # check
-fc-list | grep Monocraft # check
+fc-list | grep Monocraft 
 
 # # cursor
 # vim .Xresources
@@ -116,7 +147,6 @@ fc-list | grep Monocraft # check
 
 
 # fusuma
-sudo gem install fusuma #(?)
 sudo gpasswd -a $USER input
 newgrp input
 mkdir -p ~/.config/fusuma
@@ -129,27 +159,26 @@ sudo vim /etc/NetworkManager/conf.d/dns-servers.conf
 # # feh
 # feh --bg-scale ${imageurl}
 
-# fix monitoor setup
-# arandr to setup ONLY LAPTOP
-autorandr --save laptop
-autorandr --default laptop
-# arandr to setup EXTEND LAPTOP
-autorandr --save laptop_external
-# # arandr to setup DUPLICATE LAPTOP
-# autorandr --save laptop_duplicate
+# # fix monitoor setup
+# # arandr to setup ONLY LAPTOP
+# autorandr --save laptop
+# autorandr --default laptop
+# # arandr to setup EXTEND LAPTOP
+# autorandr --save laptop_external
+# # # arandr to setup DUPLICATE LAPTOP
+# # autorandr --save laptop_duplicate
 
-# generate ranger config
-ranger --copy-config=all
-vim .config/ranger/rc.conf
-# set show_hidden true
-# set colorscheme jungle
+# # generate ranger config
+# ranger --copy-config=all
+# vim .config/ranger/rc.conf
+# # set show_hidden true
+# # set colorscheme jungle
 
 # copy nemo
 # # export
-# dconf dump /org/nemo/ > nemo_settings.txt
-
+# dconf dump /org/nemo/ > nemo_config
 #import
-dconf load /org/nemo/ < nemo_settings.txt
+dconf load /org/nemo/ < nemo_config
 
 # copy awesome config
 mkdir -p ~/.config/awesome/
@@ -164,30 +193,31 @@ mkdir -p ~/.config/awesome/
 
 # alacritty
 mkdir -p ~/.config/alacritty/
-vim ~/.config/alacritty/alacritty.toml
 
 # vim
 mkdir -p .vim/colors/
 
 # i3lock
+cp i3lock.sh ~
 chmod +x ~/i3lock.sh
-
-# firefox
-# bookmark for calendar and mail
 
 # docker
 
 # discord
+# start discord and login to install the propper files
 # download https://betterdiscord.app/
-cd Downloads
-chmod +x BetterDiscord-Linux.AppImage
-./BetterDiscord-Linux.AppImage
+chmod +x ~/Downloads/BetterDiscord-Linux.AppImage
+~/Downloads/BetterDiscord-Linux.AppImage
 
 # wireguard
-nmcli connection import type wireguard file "/path/to/wg0.conf" # (?)
+nmcli connection import type wireguard file "/path/to/WG.conf" # !!! CHANGE PATH !!!
+nmcli connection modify WG connection.autoconnect no # !!! CHANGE NAME !!!
 
 # solo key
 # phone integration
+
+# firefox
+# bookmark for calendar and mail
 
 #--------------------------------------------
 
