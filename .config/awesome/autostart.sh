33c1527a9e4dbfb3a5a651cf637697ec1391b098
V1K1NGbg <victor@ilchev.com>
2025-01-08 17:00:31 +0100

    fix autostart and change tag order

--- .config/awesome/autostart.sh
+++ .config/awesome/autostart.sh
@@ -5,16 +5,8 @@ start() {
   then
     "$@"&
   fi
+  sleep 0.5
 }
-
-# start discord --start-minimized
-start code
-start firefox
-start discord
-start alacritty
-start nemo
-start spotify-launcher
-start keepassxc
 
 start nm-applet
 start blueman-applet
@@ -26,6 +18,15 @@ start picom -b
 start fusuma -d
 start xss-lock ~/i3lock.sh
 # start glava -d
+
+# start discord --start-minimized
+start firefox
+start discord
+start alacritty
+start nemo
+start spotify-launcher
+start keepassxc
+start code
 
 start unclutter -idle 1 -jitter 2 -root &
 ~/.bashrc
